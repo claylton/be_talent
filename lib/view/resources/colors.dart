8cc2dd12c361ba6e530c15ab9718aae006fbeed5
Claylton Santos <clayltonsp@hotmail.com>
2024-11-26 10:45:26 -0300

    ✨ feat: Add resources

---
+++ lib/view/resources/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bluePrimary = Color(0xff0500FF);
+  static const Color red = Colors.red;
+  static const Color blue10 = Color(0xffEDEFFB);
+  static const Color black = Color(0xff1c1c1c);
+  static const Color gray20 = Color(0xff9E9E9E);
+  static const Color gray10 = Color(0xffdfdfdf);
+  static const Color gray05 = Color(0xfff5f5f5);
+  static const Color gray00 = Color(0xfff0f0f0);
+  static const Color white = Colors.white;
+}
